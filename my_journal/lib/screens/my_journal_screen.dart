b4e93eb6c7811d91afbf6a08f1dd911a99b5ca4a
LTajak <lukas.tajak@sunhill-technologies.com>
2020-03-30 01:00:36 +0200

    Extended project skeleton, setup dummy screens for registration and login flow, added rounded_button custom widget

---
+++ my_journal/lib/screens/my_journal_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyJournalScreen extends StatefulWidget {
+  static String id = 'my_jounral_screen';
+
+  @override
+  _MyJournalScreenState createState() => _MyJournalScreenState();
+}
+
+class _MyJournalScreenState extends State<MyJournalScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
