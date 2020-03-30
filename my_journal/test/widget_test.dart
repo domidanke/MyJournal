ea7f61d20728bc55cc70b3e031386555326b0b39
LTajak <lukas.tajak@sunhill-technologies.com>
2020-03-30 23:42:13 +0200

    Added baseline widget tests for all screens and custom widgets

--- my_journal/test/widget_test.dart
+++ my_journal/test/widget_test.dart
@@ -7,24 +7,116 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:my_journal/main.dart';
+import 'package:my_journal/screens/login_screen.dart';
+import 'package:my_journal/screens/my_journal_screen.dart';
+import 'package:my_journal/screens/register_screen.dart';
+import 'package:my_journal/screens/welcome_screen.dart';
+import 'package:my_journal/widgets/rounded_button.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('RoundedButton is properly rendered',
+      (WidgetTester tester) async {
+    // Create RoundedButton
+    Widget roundedButton = RoundedButton(
+      text: 'Test',
+      color: Colors.lightBlueAccent,
+      onPressed: () {},
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Build testable RoundedButton using WidgetTester
+    await tester.pumpWidget(
+      MaterialApp(
+        home: roundedButton,
+      ),
+    );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Create Finders
+    final widgetFinder = find.byWidget(roundedButton);
+    final buttonTextFinder = find.text('Test');
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that RoundedButton exists and is properly rendered
+    expect(widgetFinder, findsOneWidget);
+    expect(buttonTextFinder, findsOneWidget);
+  });
+
+  testWidgets('WelcomeScreen is properly rendered',
+      (WidgetTester tester) async {
+    // Create WelcomeScreen
+    Widget welcomeScreen = WelcomeScreen();
+
+    // Build testable WelcomeScreen using WidgetTester
+    await tester.pumpWidget(
+      MaterialApp(
+        home: welcomeScreen,
+      ),
+    );
+
+    // Create Finders
+    final widgetFinder = find.byWidget(welcomeScreen);
+    final titleFinder = find.text('myJournal');
+    final registerFinder = find.text('Register');
+    final loginFinder = find.text('Log In');
+
+    // Verify that WelcomeScreen exists and is properly rendered
+    expect(widgetFinder, findsOneWidget);
+    expect(titleFinder, findsOneWidget);
+    expect(registerFinder, findsOneWidget);
+    expect(loginFinder, findsOneWidget);
+  });
+
+  testWidgets('RegisterScreen is properly rendered',
+      (WidgetTester tester) async {
+    // Create RegistrationScreen
+    Widget registrationScreen = RegistrationScreen();
+
+    // Build testable RegistrationScreen using WidgetTester
+    await tester.pumpWidget(
+      MaterialApp(
+        home: registrationScreen,
+      ),
+    );
+
+    // Create Finders
+    final widgetFinder = find.byWidget(registrationScreen);
+
+    // Verify that RegistrationScreen exists and is properly rendered
+    expect(widgetFinder, findsOneWidget);
+  });
+
+  testWidgets('LoginScreen is properly rendered', (WidgetTester tester) async {
+    // Create LoginScreen
+    Widget loginScreen = LoginScreen();
+
+    // Build testable RegistrationScreen using WidgetTester
+    await tester.pumpWidget(
+      MaterialApp(
+        home: loginScreen,
+      ),
+    );
+
+    // Create Finders
+    final widgetFinder = find.byWidget(loginScreen);
+
+    // Verify that LoginScreen exists and is properly rendered
+    expect(widgetFinder, findsOneWidget);
+  });
+
+  testWidgets('MyJournalScreen is properly rendered',
+      (WidgetTester tester) async {
+    // Create RegistrationScreen
+    Widget myJournalScreen = MyJournalScreen();
+
+    // Build testable RegistrationScreen using WidgetTester
+    await tester.pumpWidget(
+      MaterialApp(
+        home: myJournalScreen,
+      ),
+    );
+
+    // Create Finders
+    final widgetFinder = find.byWidget(myJournalScreen);
+
+    // Verify that MyJournalScreen exists and is properly rendered
+    expect(widgetFinder, findsOneWidget);
   });
 }
