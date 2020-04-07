45bab07596d57a435467a69d6c1f4febf758066a
LTajak <lukas.tajak@gmail.com>
2020-04-07 14:28:12 +0200

    Added error handling UX at registration.

--- my_journal/lib/screens/registration_screen.dart
+++ my_journal/lib/screens/registration_screen.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:my_journal/screens/my_journal_screen.dart';
@@ -17,6 +18,48 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   bool showSpinner = false;
   String email;
   String password;
+
+  void alertUser(String alertTitle, String alertMessage) {
+    showDialog(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        if (Theme.of(context).platform == TargetPlatform.android) {
+          return AlertDialog(
+            title: Text(alertTitle),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text(alertMessage),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        } else {
+          return CupertinoAlertDialog(
+            title: Text(alertTitle),
+            content: Text(alertMessage),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        }
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,6 +125,39 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                     });
                     // TODO: Show error message to user
                     print(e);
+                    switch (e.code) {
+                      case 'ERROR_INVALID_EMAIL':
+                        {
+                          alertUser(
+                            'Registration failed',
+                            'Please enter a valid email address.',
+                          );
+                        }
+                        break;
+                      case 'ERROR_WEAK_PASSWORD':
+                        {
+                          alertUser(
+                            'Registration failed',
+                            'The password must be at least 6 characters long.',
+                          );
+                        }
+                        break;
+                      case 'ERROR_EMAIL_ALREADY_IN_USE':
+                        {
+                          alertUser(
+                            'Registration failed',
+                            'The email address is already in use by another account.',
+                          );
+                        }
+                        break;
+                      default:
+                        {
+                          alertUser(
+                            'Registration failed',
+                            'Something went wrong. Please try again later.',
+                          );
+                        }
+                    }
                   }
                 },
               ),
