dd983a8f105caffbe463ae9b4ec42c9db55bd08f
LTajak <lukas.tajak@gmail.com>
2020-04-07 16:35:06 +0200

    Added keys to TextFields to find them byKey in test classes.

--- my_journal/test/login_screen_test.dart
+++ my_journal/test/login_screen_test.dart
@@ -24,16 +24,41 @@ void main() {
 
     // Create Finders
     final widgetFinder = find.byWidget(loginScreen);
-    final roundedButtonFinder = find.byType(RoundedButton(
-      onPressed: () {},
-    ).runtimeType);
+    final roundedButtonFinder = find.byType(RoundedButton);
     final registerFinder = find.text('Log In');
-    final textFieldFinder = find.byType(TextField().runtimeType);
+    final textFieldFinder = find.byType(TextField);
+    final emailTextFieldFinder = find.byKey(Key('email'));
+    final passwordTextFieldFinder = find.byKey(Key('password'));
 
     // Verify that LoginScreen exists and is properly rendered
     expect(widgetFinder, findsOneWidget);
     expect(roundedButtonFinder, findsOneWidget);
     expect(registerFinder, findsOneWidget);
     expect(textFieldFinder, findsNWidgets(2));
+    expect(emailTextFieldFinder, findsOneWidget);
+    expect(passwordTextFieldFinder, findsOneWidget);
+  });
+
+  testWidgets('Log In alerts are properly rendered',
+      (WidgetTester tester) async {
+    // Create RegistrationScreen
+    Widget registrationScreen = LoginScreen();
+
+    // Build testable LoginScreen using WidgetTester
+    await tester.pumpWidget(
+      MaterialApp(
+        home: registrationScreen,
+      ),
+    );
+
+    // Enter invalid email
+    await tester.enterText(find.byKey(Key('email')), 'lukas.tajak');
+    await tester.enterText(find.byKey(Key('password')), 'test123');
+
+    // Tap the RoundedButton
+    await tester.tap(find.text('Log In'));
+
+    // Rebuild the widget after the state has changed.
+    await tester.pump();
   });
 }
