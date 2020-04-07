7be79d39a377ee92ed6e38a207179e61c532c96b
LTajak <lukas.tajak@gmail.com>
2020-04-07 21:52:55 +0200

    Extracted logic from platform-specific alerts into separate class. Added widget test for new class. Added todos in existing widget tests.

--- my_journal/test/registration_screen_test.dart
+++ my_journal/test/registration_screen_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -63,4 +56,6 @@ void main() {
     // Rebuild the widget after the state has changed.
     await tester.pump();
   });
+
+  //Todo: Test positive and negative resgistration scenarios
 }
