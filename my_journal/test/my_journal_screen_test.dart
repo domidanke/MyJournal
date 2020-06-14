e0e3e7e7af27a49e844c7e495fdc0b3e897352b5
Dominik Tajak <dominik.tajak@gmx.de>
2020-06-13 22:19:03 -0500

    Added More Testing

--- my_journal/test/my_journal_screen_test.dart
+++ my_journal/test/my_journal_screen_test.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
 import 'package:mockito/mockito.dart';
 import 'package:my_journal/screens/create_entry.dart';
 import 'package:my_journal/screens/my_journal_screen.dart';
 import 'package:my_journal/widgets/home_card.dart';
+import 'package:my_journal/widgets/rounded_button.dart';
 
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
@@ -18,7 +20,8 @@ void main() {
     expect(find.byType(HomeCard), findsNWidgets(3));
   });
 
-  testWidgets('HomeCard 1 is clickable and navigates properly',
+  testWidgets(
+      'HomeCard 1 is clickable, navigates properly, and renders Create Entry correctly',
       (WidgetTester tester) async {
     final mockObserver = MockNavigatorObserver();
     await tester.pumpWidget(
@@ -38,7 +41,15 @@ void main() {
 
     /// Navigated page is present in the screen.
     expect(find.byType(CreateEntry), findsOneWidget);
-    expect(find.byType(IconButton), findsNWidgets(6));
-    expect(find.text('How Did you feel?'), findsOneWidget);
+    expect(
+        find.text(DateFormat.yMMMd().format(DateTime.now())), findsOneWidget);
+    expect(find.byIcon(Icons.date_range), findsOneWidget);
+    expect(find.text('Change Date'), findsOneWidget);
+    expect(find.text('How Did You feel?'), findsOneWidget);
+    expect(find.byType(IconButton), findsNWidgets(7));
+    expect(find.byType(TextField), findsNWidgets(2));
+    expect(find.text('Special Day'), findsOneWidget);
+    expect(find.byType(RoundedButton), findsOneWidget);
+    expect(find.text('Save Journal Entry'), findsOneWidget);
   });
 }
